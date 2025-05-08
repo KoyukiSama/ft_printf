c46ac85d8ef682b9473a5839116971f3786a5456
Katarina <kateriiner@cute-huisbaas.localdomain>
2025-05-08 23:33:35 +0200

    add (ft_itoa_base.c), conversion done

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,10 +6,11 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/08 22:40:21 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/05/08 23:04:16 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/05/08 23:32:12 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
 #include "ft_printf.h"
 
 char	*ft_itoa_base_signed(int val, char *base, bool *sign)
@@ -30,9 +31,17 @@ char	*ft_itoa_base_unsigned(unsigned int val, char *base)
 {
 	char	buffer[35];
 	char	*str;
+	size_t	base_len;
 	
+	if (val == 0)
+		return (ft_strdup("0"));
+	base_len = ft_strlen(base);
 	str = buffer + (sizeof(buffer) - 1);
 	*str = '\0';
-
-	// TODO
+	while (val > 0)
+	{
+		*--str = base[val % base_len];
+		val /= base_len;
+	}
+	return (ft_strdup(str));
 }
