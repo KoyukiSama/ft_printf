c67b975243e813023ae770f2fd57d41c9d0550d3
Katarina Claes <kclaes@f0r1s13.codam.nl>
2025-05-10 15:04:38 +0200

    ADD (test/printfplayground), more printf edge cases to take into account

--- test/printf_playground.c
+++ test/printf_playground.c
@@ -4,43 +4,37 @@
 
 int main(void)
 {
-	printf("%01d\n", 42);
-	printf("%.1d\n\n", 42);
-
-	printf("%02d\n", 42);
-	printf("%.2d\n\n", 42);
-
-	printf("%03d\n", 42);
-	printf("%.3d\n\n", 42);
-
-	printf("%.4d\n", 42);
-	printf("%.4d\n\n", 42);
+	// %c
+	//printf("|%.3c|\n", 'c'); 		// WRONG .3c
+	//printf("|%03c|\n", 'c'); 		// WRONG 03c
+	printf(" 3c -> |%3c|\n", 'c');  // RIGHT  3c
+	printf("-3c -> |%-3c|\n", 'c'); // RIGHT -3c
+	printf("-3c -> |%-3c|\n\n", '\0'); // 
 	
-	printf("%.5d\n", 42);
-	printf("%.5d\n\n", 42);
-
-	printf("%x\n", -8);
-
-	// negative numbers: precision pads only the digits, width+0 pads the whole field
-	printf("|%05d|\n", -42);  // |-0042|   width=5 includes the “-” so you get 2 zeros  
-	printf("|%.5d|\n", -42);  // |-00042|  precision=5 applies to the “42” only, then sign  
-
-	// zero value with precision zero vs zero‐flag
-	printf("|%0d|\n", 0);     // |0|       no width specified, zero‐flag ignored, prints “0”  
-	printf("|%.0d|\n", 0);    // ||        precision=0 + value=0 → prints nothing  
-
+	char *hi = NULL;
 	// strings: precision truncates, zero‐flag is ignored  
-	printf("|%.3s|\n", "hello"); // |hel|  
-	printf("|%3s|\n", "hi");    // |  hi|   zero‐flag doesn’t pad strings, so spaces
+	printf(".3s -> |%.3s|\n", "hello"); // |hel|  
+	printf(" 3s -> |%3s|\n", "hi");      // |  hi|   zero‐flag doesn’t pad strings, so spaces
+	printf("-3s -> |%3s|\n", "hi");      
+	printf("-3s -> |%3s|\n", "\0");      
+	printf("-3s -> |%3s|\n\n", hi);      
 
-	//printf("|%.3c|\n", 'c');
+	// %d %i
+	printf("05d ->  |%05d|\n", 42);
+	printf(".5d ->  |%.5d|\n", 42);
+	printf("05d ->  |%05d|\n", -42);  // |-0042|   width=5 includes the “-” so you get 2 zeros  
+	printf(".5d -> |%.5d|\n", -42);  // |-00042|  precision=5 applies to the “42” only, then sign  
+	printf(" 0d -> |%0d|\n", 0);     // |0|       no width specified, zero‐flag ignored, prints “0”  
+	printf(".0d -> |%.0d|\n\n", 0);    // ||        precision=0 + value=0 → prints nothing
 
-	//char hello[] = "hello";
-	//printf("|%.3p|\n", hello);
+	char hello[] = "hi";
+	// %p
+	printf("p -> %p\n", hello);
+	printf("p -> %p\n", NULL);
+	//printf("%05p", hello); // 0 flag %p WRONG
+	//printf("%.5p", hello); // . flag %p WRONG
 
-	//printf("|hello%3%|\n");
+	
 
-	// combine width+precision to see zero‐flag vanish
-	printf("|%08d|\n",   123); // |00000123|  zero-flag pads to width when no precision  
 	return (0);
 }
