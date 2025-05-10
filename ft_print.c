b509d5fb54a0d6ec9e78b1cc5a264817ee865364
Katarina Claes <kclaes@f0r1s13.codam.nl>
2025-05-10 16:03:37 +0200

    add (ft_printf.c), ft_find_s_end

--- ft_print.c
+++ ft_print.c
@@ -6,13 +6,40 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/08 22:10:09 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/05/10 15:24:16 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/05/10 16:02:23 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
 
 void	ft_printf(char *s, ...)
 {
-	
+	size_t	len;
+	char	*s_end;
+
+	len = 0;
+	while (*s)
+	{
+		s_end = ft_find_s_end(s);
+		
+		len = s_prcnt - s;
+	}
+}
+
+// get's the next % or '\0' if not found
+char	*ft_find_s_end(char *s)
+{
+	char	*s_prcnt;
+
+	s_prcnt = ft_strchr(s, '%');
+	if (s_prcnt == NULL)
+		return (ft_strchr(s, '\0'));
+	return (s_prcnt);
+}
+
+void	ft_printn(char *s, size_t len)
+{
+	write(1, s, len);
 }
