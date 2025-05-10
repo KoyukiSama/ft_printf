fe2745ca446a811d5419a2eb08e4f9e506f39cbb
Katarina Claes <kclaes@f0r1s13.codam.nl>
2025-05-10 15:10:59 +0200

    add (ft_printf.h), changed flags around

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/08 22:09:39 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/05/08 22:28:15 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/05/10 15:09:58 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 // "      42" = right_justf
 // "42      " = left_justf
-// "00000042" = digits_width
-// "-0000042" = nbr_width
+// "00000042" = perc_flag
+// "-0000042" = 0_flag
 typedef struct s_flags
 {
-	bool	right_justf;
-	bool	left_justf;
-	bool	digits_width;
-	bool	nbr_width;
+	int		right_justf;
+	int		left_justf;
+	int		zeros_width;
+	bool	zero_flag;
+	bool	perc_flag;
 	bool	nbr_neg;
 	bool	nbr_zero;
 }	t_flags;
