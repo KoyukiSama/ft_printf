cf5cda0c3802ae8a084a65d281e2397fc2766241
Katarina <kateriiner@cute-huisbaas.localdomain>
2025-05-20 20:11:59 +0200

    fix (printf_test1.c),

--- test/printf_test1.c
+++ test/printf_test1.c
@@ -9,7 +9,7 @@ int main(void)
 	//ft_printf("len: %i ✅\n\n", len); // if you want to check length
 	//ft_printf(PPL"<3-----[TESTS 1]: %%c testing-----<3\n"RESET);
 	//ft_printf("|%-5d|\n", 59205);
-	ft_printf("|%c, %c, %%, %s, %-5s, %d, %d|\n", 'a', 'b', "hello", "hello", 52189, 59205);
+	ft_printf("|%c, %c, %%, %s, %s, %d, %02d, %x|\n", 'a', 'b', "hello", "hello", 52189, 59205, 255);
 	// ft_printf("|%.5c|\n", 'a');
 	// ft_printf("|%05c|\n", 'a');
 	// ft_printf("|%5c|\n", 'a');
